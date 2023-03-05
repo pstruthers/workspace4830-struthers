e31d0ffa3f44f58b232fb1b5995cb0e7fd92edd2
paige <pstruthers@unomaha.edu>
2023-03-05 17:58:23 -0600

    Initial Commits.

---
+++ webpro/src/main/java/InsertEmployeeDetails.java
@@ -0,0 +1,111 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+// Fetch data
+/**
+ * Servlet implementation class demo3
+ */
+@WebServlet("/InsertEmployeeDetails")
+public class InsertEmployeeDetails extends HttpServlet {
+    private static final long serialVersionUID = 1;
+
+    String dns = "ec2-13-59-14-24.us-east-2.compute.amazonaws.com";
+
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InsertEmployeeDetails() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        String sql;
+        Connection connection = null;
+        PreparedStatement statement1 = null;
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String phonenumber = request.getParameter("phonenumber");
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String title = "Insert Employee Details into database";
+        String docType =
+            "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+
+        out.println(docType + //
+            "<html>\n" + //
+            "<head><title>" + title + "</title></head>\n" + //
+            "<body bgcolor = \"##CCCCFF\">\n" + //
+            "<h1 align = \"center\">" + title + "</h1>\n");
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            return;
+        }
+
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://" + dns + ":3306/myDB1", "admin1", "root");
+        } catch (SQLException e2) {
+            // TODO Auto-generated catch block
+            System.out.println("Connection Failed!:\n" + e2.getMessage());
+        }
+        System.out.println("SUCCESS!!!! You made it, take control     your database now!");
+        System.out.println("Creating statement...");
+
+        sql = "insert into myTable (MYUSER,EMAIL,PHONE) values(?,?,?);";
+
+        try {
+
+            statement1 = connection.prepareStatement(sql);
+            String theUserName = username;
+            String emailval = email;
+            String phoneval = phonenumber;
+            statement1.setString(1, theUserName);
+            statement1.setString(2, emailval);
+            statement1.setString(3, phoneval);
+
+        } catch (SQLException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
+        }
+
+        try {
+
+            statement1.executeUpdate();
+        } catch (SQLException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        out.println("Thank you for registering your details");
+        out.println("</body></html>");
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
